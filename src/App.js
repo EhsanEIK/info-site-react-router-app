14b01d16a3961ee5c2544eb059157e375258c90b
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-04 13:56:30 +0600

    added navbar design using tailwind css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import router from './routers/routers';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Header.css';
 
 const Header = () => {
     return (
-        <nav>
-            <NavLink className={({ isActive }) => isActive ? 'blue' : undefined} to='/home'>Home</NavLink>
-            <NavLink className={({ isActive }) => isActive ? 'blue' : undefined} to='/users'>Users</NavLink>
-            <NavLink className={({ isActive }) => isActive ? 'blue' : undefined} to='/blogs'>Blogs</NavLink>
-            <NavLink className={({ isActive }) => isActive ? 'blue' : undefined} to='/countries'>Country</NavLink>
-        </nav >
+        <nav className='bg-teal-600 text-white flex justify-end p-6'>
+            <NavLink className={({ isActive }) => isActive ? 'bg-blue-700 mr-5 p-1' : 'mr-5 p-1'} to='/home'>Home</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'bg-blue-700 mr-5 p-1' : 'mr-5 p-1'} to='/users' > Users</NavLink >
+            <NavLink className={({ isActive }) => isActive ? 'bg-blue-700 mr-5 p-1' : 'mr-5 p-1'} to='/blogs'>Blogs</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'bg-blue-700 mr-5 p-1' : 'mr-5 p-1'} to='/countries'>Country</NavLink>
+        </nav>
     );
 };
 
