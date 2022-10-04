36e07c21f6aee1e8d9f20b63e0765bf73676e3ad
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-04 19:33:26 +0600

    added userdetails page design

--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -6,13 +6,15 @@ const UserDetails = () => {
     const { name, username, email, phone, company, address, website } = userDetails;
     return (
         <div>
-            <h2>Here are all details of {name}</h2>
-            <h3>Email: {email}</h3>
-            <h3>Phone: {phone}</h3>
-            <h3>Website: {website}</h3>
-            <h3>Company: {company.name}</h3>
-            <h3>Address: {address.city}</h3>
-            <h3>User-Name: {username}</h3>
+            <h2 className='text-center text-4xl mb-5'>Here are all details of {name}</h2>
+            <div className='text-center text-lg'>
+                <h3><span className='font-bold mr-2'>Email:</span> {email}</h3>
+                <h3><span className='font-bold mr-2'>Phone:</span> {phone}</h3>
+                <h3><span className='font-bold mr-2'>Website:</span> {website}</h3>
+                <h3><span className='font-bold mr-2'>Company:</span> {company.name}</h3>
+                <h3><span className='font-bold mr-2'>Address:</span> {address.city}</h3>
+                <h3><span className='font-bold mr-2'>User-Name:</span> {username}</h3>
+            </div>
         </div>
     );
 };
