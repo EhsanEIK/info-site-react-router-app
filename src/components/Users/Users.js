4d9129f97ea4bbdb450195964cddcfcf80a41122
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-04 09:39:22 +0600

    initial commit

---
+++ src/components/Users/Users.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <div>
+            <h2>Welcome to the users page</h2>
+        </div>
+    );
+};
+
+export default Users;
