dc7d8c520b339c6eab6f528b0e0f82b60a87ff6a
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-04 11:27:49 +0600

    added active navlink system in Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
         <nav>
-            <Link to='/home'>Home</Link>
-            <Link to='/users'>Users</Link>
-            <Link to='/blogs'>Blogs</Link>
-            <Link to='/countries'>Country</Link>
-        </nav>
+            <NavLink className={({ isActive }) => isActive ? 'blue' : undefined} to='/home'>Home</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'blue' : undefined} to='/users'>Users</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'blue' : undefined} to='/blogs'>Blogs</NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'blue' : undefined} to='/countries'>Country</NavLink>
+        </nav >
     );
 };
 
