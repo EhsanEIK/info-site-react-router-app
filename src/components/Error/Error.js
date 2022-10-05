260c9c8126e001a7c3567ad05408e403d8332395
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-05 21:28:04 +0600

    added error 404 pages

---
+++ src/components/Error/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className='bg-red-500 text-white rounded-xl p-10 m-20'>
+            <h2 className='text-4xl mb-5'>ERROR 404 FOUND!!</h2>
+            <p className='text-lg'>There is nothing on this route.</p>
+        </div>
+    );
+};
+
+export default Error;
