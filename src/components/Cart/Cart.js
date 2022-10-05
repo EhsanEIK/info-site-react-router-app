f5aa133757d95d58d50e06a698f12dc81af1e9cf
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-05 14:22:04 +0600

    showd cart items on cart section

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 const Cart = ({ cart }) => {
-    let quantity = 0;
+    console.log(cart)
+    let totalItems = 0;
     for (const item of cart) {
-        quantity += item.quantity;
+        totalItems += item.quantity;
     }
     return (
         <div className='sticky'>
             <h2 className='text-4xl text-amber-400 font-bold mb-2'>Order Cart</h2>
             <hr />
-            <div className='text-lg text-white mt-5'>
-                <p>Total Items: {quantity}</p>
+            <div className='text-white mt-5'>
+                <p className='text-xl font-semibold mb-2'>Total Items: {totalItems}</p>
+                <ol className='text-lg list-decimal pl-5'>
+                    {
+                        cart.map(item =>
+                            <li>{item.strMeal} : <span className='bg-red-400 text-base text-blue-700 font-bold border-2 rounded-md px-2'>{item.quantity}</span></li>)
+                    }
+                </ol>
             </div>
         </div>
     );
